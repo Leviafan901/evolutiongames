33437d769fb521e2ff068e1e352a482bce84b489
Aliaksei Sosenkov <Aliaksei_Sosenkov@epam.com>
2020-06-01 01:15:30 +0300

    Has created a base model with accuracy 71%

--- src/insurance/__init__.py
+++ src/insurance/__init__.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from scipy.stats import skew
+from sklearn.linear_model import LinearRegression
 
 if __name__ == "__main__":
     train = pd.read_csv("../input/train.csv")
@@ -39,3 +40,12 @@ if __name__ == "__main__":
     ## Train test split follows this distinguished code pattern and helps creating train and test set to build machine learning.
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.33, random_state=0)
     print(X_train.shape, y_train.shape, X_test.shape, y_test.shape)
+
+    ## Call in the LinearRegression object
+    lin_reg_model = LinearRegression(normalize=True, n_jobs=-1)
+    ## fit train and test data.
+    lin_reg_model.fit(X_train, y_train)
+    ## Predict test data.
+    y_pred = lin_reg_model.predict(X_test)
+    accuracy = lin_reg_model.score(X_test, y_test)
+    print(accuracy)
