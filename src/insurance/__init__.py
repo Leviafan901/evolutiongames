31676c51364efe83750f4a33a173ec7c6887603c
Aliaksei Sosenkov <Aliaksei_Sosenkov@epam.com>
2020-06-01 00:53:20 +0300

    Many print calls, features are developed, need to fix their quality and size

--- src/insurance/__init__.py
+++ src/insurance/__init__.py
@@ -1,10 +1,12 @@
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
+from scipy.stats import skew
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.metrics import classification_report, confusion_matrix
 import seaborn as sns
+#from ploting import distribution as dis_plot
 
 if __name__ == "__main__":
     train = pd.read_csv("../input/train.csv")
@@ -20,16 +22,27 @@ if __name__ == "__main__":
     print("Skewness: " + str(train['charges'].skew()))
     print("Kurtosis: " + str(train['charges'].kurt()))
 
-    print((train.corr() ** 2)["charges"].sort_values(ascending=False)[1:])
+    ## Saving the target values in "y_train".
+    y = train['charges'].reset_index(drop=True)
 
-    #normal distribution
-    train["charges"] = np.log1p(train["charges"])
-    #plotting_3_chart(train, 'charges')
+    all_data = pd.concat((train, test)).reset_index(drop=True)
+    ## Dropping the target variable.
+    all_data.drop(['charges'], axis=1, inplace=True)
 
-    sns.distplot(train['smoker'])
+    numeric_feats = all_data.dtypes[all_data.dtypes != "object"].index
+    skewed_feats = all_data[numeric_feats].apply(lambda x: skew(x)).sort_values(ascending=False)
 
-    dc.fixing_skewness(train)
-    dc.fixing_skewness(test)
-    print("Skewness: " + str(train['charges'].skew()))
-    final_features = pd.get_dummies(train).reset_index(drop=True)
+    print(skewed_feats)
+
+    dc.fixing_skewness(all_data)
+
+    print(all_data.shape)
+    final_features = pd.get_dummies(all_data).reset_index(drop=True)
     print(final_features.shape)
+    X = final_features.iloc[:len(y), :]
+    X_sub = final_features.iloc[len(y):, :]
+
+    from sklearn.model_selection import train_test_split
+    ## Train test split follows this distinguished code pattern and helps creating train and test set to build machine learning.
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.33, random_state=0)
+    print(X_train.shape, y_train.shape, X_test.shape, y_test.shape)
