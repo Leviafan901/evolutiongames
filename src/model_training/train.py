c9502f48c3a6d715d1f56f098cd1608526bead64
Aliaksei Sosenkov <Aliaksei_Sosenkov@epam.com>
2020-06-02 01:47:19 +0300

    Has removed the KNeighbor and impoved the linear regression

--- src/model_training/train.py
+++ src/model_training/train.py
@@ -8,11 +8,3 @@ def linear_regression(X_train, X_test, y_train, y_test):
     lin_reg_model.predict(X_test)
     accuracy = lin_reg_model.score(X_test, y_test)
     print(f'Linear regression accuracy = %.2f' % accuracy)
-
-
-def neighbor_regression(X_train, X_test, y_train, y_test):
-    model = KNeighborsRegressor(n_neighbors=5)
-    model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
-    accuracy = model.score(X_test, y_test)
-    print(f'KNeighbors regression accuracy = %.2f' % accuracy)
