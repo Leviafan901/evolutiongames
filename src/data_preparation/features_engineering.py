423c039908d22a9689fbace69abb0adaa9f58d5c
Aliaksei Sosenkov <Aliaksei_Sosenkov@epam.com>
2020-06-01 16:34:50 +0300

    Minor name convension refactoring

--- src/data_preparation/features_engineering.py
+++ src/data_preparation/features_engineering.py
@@ -7,13 +7,13 @@ TARGET_PREDICTION_VALUE = 'charges'
 
 
 def final_sets(data):
-    ## Saving the target values in y
-    y = data.train_set[TARGET_PREDICTION_VALUE].reset_index(drop=True)
+    ## Saving the target values in to_predict
+    to_predict = data.train_set[TARGET_PREDICTION_VALUE].reset_index(drop=True)
     all_data = pd.concat((data.train_set, data.test_set)).reset_index(drop=True)
     ## Dropping the target variable.
     all_data.drop([TARGET_PREDICTION_VALUE], axis=1, inplace=True)
     data_cleaning.fixing_skewness(all_data)
     final_features = pd.get_dummies(all_data).reset_index(drop=True)
-    X = final_features.iloc[:len(y), :]
+    random_data_subsets = final_features.iloc[:len(to_predict), :]
     ## Train test split follows this distinguished code pattern and helps creating train and test sets
-    return train_test_split(X, y, test_size=.33, random_state=0)
+    return train_test_split(random_data_subsets, to_predict, test_size=.33, random_state=0)
